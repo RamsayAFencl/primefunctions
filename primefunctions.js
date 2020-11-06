92a51bb9243664ebf6bf31b7cf514424c7c685cf
RamsayAFencl <71794894+RamsayAFencl@users.noreply.github.com>
2020-11-05 23:35:04 -0800

    Finished cumulativeSum
    works as expected

--- primefunctions.js
+++ primefunctions.js
@@ -16,4 +16,16 @@ function primeGen(num) {
   return prime;
 }
 
+function cumulativeSum(arr){
+  let sum = 0;
+  const ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    sum += a;
+    ret.push(sum);
+  }
+  return ret;
+}
+
 console.log(primeGen(10));
+console.log(cumulativeSum([1, 2, 3, 4, 5, 6]));
