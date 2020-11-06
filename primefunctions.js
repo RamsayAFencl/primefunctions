15abe0e2e21e1aab8e4589f5159c130aea35440a
RamsayAFencl <71794894+RamsayAFencl@users.noreply.github.com>
2020-11-05 23:18:35 -0800

    Finished primeGen
    functions properly

--- primefunctions.js
+++ primefunctions.js
@@ -2,3 +2,18 @@
 Ramsay Fencl
 Fundamentals HW 4
  */
+function primeGen(num) {
+  const inList = [];
+  const prime = [];
+  for (let i = 2; i <= num; ++i) {
+    if (!inList[i]) {
+      prime.push(i);
+      for (let j = i * 2; j <= num; j += i) {
+        inList[j] = true;
+      }
+    }
+  }
+  return prime;
+}
+
+console.log(primeGen(10));
