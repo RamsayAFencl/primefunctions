28d051faa5adbe83d5016542c6ee714232a92e1e
RamsayAFencl <71794894+RamsayAFencl@users.noreply.github.com>
2020-12-09 18:46:41 -0800

    Update testCorrectness.js

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,1 +1,10 @@
-
+describe('primeGen(10)', function () {
+  it('must equal [2,3,5,7]', function () {
+    chai.expect(primeGen(10)).to.deep.equal([2, 3, 5, 7]);
+  });
+});
+describe('primeGen(20)', function () {
+  it('must equal [2,3,5,7,11,13,17,19]', function () {
+    chai.expect(primeGen(20)).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
+  });
+});
