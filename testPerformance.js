a95d012a9f5fd38ad01b86a53248df52719a240c
RamsayAFencl <71794894+RamsayAFencl@users.noreply.github.com>
2020-12-09 22:41:43 -0800

    finished testPerformance

--- testPerformance.js
+++ testPerformance.js
@@ -1,1 +1,15 @@
+describe('maxPrimeSum', () => {
+  it('maxPrimeSum(10000) is faster than 50ms', function () {
+    this.slow(0);
+    this.timeout(50);
+    // eslint-disable-next-line no-undef
+    maxPrimeSum(10000);
+  });
 
+  it('maxPrimeSum(100000) is faster than 3000ms', function () {
+    this.slow(0);
+    this.timeout(3000);
+    // eslint-disable-next-line no-undef
+    maxPrimeSum(100000);
+  });
+});
