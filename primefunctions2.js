44286547a1a6dfbae0d490e5d92d4d6bfbdff5cc
RamsayAFencl <71794894+RamsayAFencl@users.noreply.github.com>
2020-12-09 20:40:51 -0800

    Update primefunctions2.js

--- primefunctions2.js
+++ primefunctions2.js
@@ -1,1 +1,43 @@
+/*
+Ramsay Fencl
+Fundamentals HW 4
+ */
+function primeGen(num) {
+  const inList = [];
+  const prime = [];
+  for (let i = 2; i <= num; ++i) {
+    if (!inList[i]) {
+      prime.push(i);
+      for (let j = i * 2; j <= num; j += i) {
+        inList[j] = true;
+      }
+    }
+  }
+  return prime;
+}
+
+function cumulativeSum(arr) {
+  let sum = 0;
+  const ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    sum += a;
+    ret.push(sum);
+  }
+  return ret;
+}
+
+function maxPrimeSum(num) {
+  const ret = [];
+  const prime = primeGen(num);
+  const sumList = cumulativeSum(prime);
+  const sum = sumList.pop();
+  ret.push(sum);
+  ret.push(prime.length);
+  return ret;
+}
+
+// console.log(primeGen(10));
+// console.log(cumulativeSum([1, 2, 3, 4, 5, 6]));
+// console.log(maxPrimeSum(10));
 
